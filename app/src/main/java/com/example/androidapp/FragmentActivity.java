bbfc5997b4a55ec07e746e5e98a6773a980212d7
NiakhaichykVitaliy <2603hbo@gmail.com>
2019-06-17 16:07:25 +0300

    Create FragmentsActivity

---
+++ app/src/main/java/com/example/androidapp/FragmentActivity.java
@@ -0,0 +1,51 @@
+package com.example.androidapp;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FragmentActivity extends AppCompatActivity {
+
+    private Button firstFragment, secondFragment;
+
+    public static void openActivity(Context context) {
+        Intent intent = new Intent(context, FragmentActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        firstFragment = findViewById(R.id.first_fragment);
+        secondFragment = findViewById(R.id.second_fragment);
+
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FirstFragment());
+            }
+        });
+
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new SecondFragment());
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
