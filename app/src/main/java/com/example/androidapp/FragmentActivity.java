3279f9f6fa84a943f6a5142d51b761bb8674305e
NiakhaichykVitaliy <2603hbo@gmail.com>
2019-06-24 18:07:31 +0300

    bottom navigation menu (v2)

--- app/src/main/java/com/example/androidapp/FragmentActivity.java
+++ app/src/main/java/com/example/androidapp/FragmentActivity.java
@@ -4,11 +4,10 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
 public class FragmentActivity extends AppCompatActivity {
 
@@ -17,43 +16,34 @@ public class FragmentActivity extends AppCompatActivity {
         context.startActivity(intent);
     }
 
-    private TextView textHome;
-    private TextView textFavorites;
-    private TextView textSettings;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
-        textHome = findViewById(R.id.text_home);
-        textFavorites = findViewById(R.id.text_favorites);
-        textSettings = findViewById(R.id.text_settings);
-
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.action_home:
-                                textHome.setVisibility(View.VISIBLE);
-                                textFavorites.setVisibility(View.GONE);
-                                textSettings.setVisibility(View.GONE);
-                                break;
-                            case R.id.action_favorite:
-                                textHome.setVisibility(View.GONE);
-                                textFavorites.setVisibility(View.VISIBLE);
-                                textSettings.setVisibility(View.GONE);
-                                break;
-                            case R.id.action_settings:
-                                textHome.setVisibility(View.GONE);
-                                textFavorites.setVisibility(View.GONE);
-                                textSettings.setVisibility(View.VISIBLE);
-                                break;
-                        }
-                        return false;
-                    }
-                });
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationListner);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationListner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.action_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.action_favorite:
+                            selectedFragment = new FavoritesFragment();
+                            break;
+                        case R.id.action_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
