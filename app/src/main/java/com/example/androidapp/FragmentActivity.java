5304372cf335729eb526a853b9e10c27348a6abe
NiakhaichykVitaliy <2603hbo@gmail.com>
2019-06-25 12:22:00 +0300

    some fix fragment.xml

--- app/src/main/java/com/example/androidapp/FragmentActivity.java
+++ app/src/main/java/com/example/androidapp/FragmentActivity.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -46,7 +45,7 @@ public class FragmentActivity extends AppCompatActivity {
     private void loadFragment(Fragment fragment) {
         FragmentManager fm = getSupportFragmentManager();
         fm.beginTransaction()
-                .replace(R.id.fragment, fragment)
+                .replace(R.id.frame_layout, fragment)
                 .commit();
     }
 }
