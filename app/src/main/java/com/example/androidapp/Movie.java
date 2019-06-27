081a7e1ed9b0a14f4ecdcbb50e3762af79ba2e4b
NiakhaichykVitaliy <2603hbo@gmail.com>
2019-06-27 18:39:41 +0300

    some code refactoring

--- app/src/main/java/com/example/androidapp/Movie.java
+++ app/src/main/java/com/example/androidapp/Movie.java
@@ -5,6 +5,30 @@ public class Movie {
     private String description;
     private String imageUrl;
 
-    public Movie(String title) {
+    public Movie(String title, String description) {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
--- app/src/main/java/com/example/androidapp/TestMoviesRepositoryImpl.java
+++ app/src/main/java/com/example/androidapp/TestMoviesRepositoryImpl.java
@@ -8,9 +8,14 @@ public class TestMoviesRepositoryImpl implements MoviesRepository {
     @Override
     public List<Movie> getMovies() {
         List<Movie> movies = new ArrayList<>();
-        movies.add(new Movie("god bless the queen"));
-        movies.add(new Movie("Елизавета II обожает собак породы корги"));
-        movies.add(new Movie("К королеве нельзя прикасаться"));
+        movies.add(new Movie("god bless the queen","1"));
+        movies.add(new Movie("Елизавета II обожает собак породы корги", "12"));
+        movies.add(new Movie("К королеве нельзя прикасаться","123"));
+        movies.add(new Movie("Idi nahui","1234"));
+        movies.add(new Movie("Vse esche idi nahui", "12345"));
+        movies.add(new Movie("Ladno, idi v pizdy","123456"));
+        movies.addAll(movies);
+        movies.addAll(movies);
         return movies;
     }
 }
