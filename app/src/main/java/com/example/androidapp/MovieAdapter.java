98298253667756f99f599f21ada944c4708b739e
NiakhaichykVitaliy <2603hbo@gmail.com>
2019-07-01 18:38:32 +0300

    recycler view

---
+++ app/src/main/java/com/example/androidapp/MovieAdapter.java
@@ -0,0 +1,53 @@
+package com.example.androidapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    private List<Movie> movies;
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_row, parent, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        holder.bind(movies.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies == null ? 0 : movies.size();
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder {
+        private TextView movieTitleTextView;
+        private TextView movieDescriptionTextView;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            movieTitleTextView = itemView.findViewById(R.id.movie_title_view);
+            movieDescriptionTextView = itemView.findViewById(R.id.movie_description_view);
+        }
+
+        public void bind(Movie text) {
+            movieTitleTextView.setText(text.getTitle());
+            movieDescriptionTextView.setText(text.getDescription());
+        }
+    }
+}
