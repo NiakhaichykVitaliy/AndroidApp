d6361e68341ca1491f2735d5995e78e355f48fdc
NiakhaichykVitaliy <2603hbo@gmail.com>
2019-07-10 15:06:02 +0300

    refactoring recycler

--- app/src/main/java/com/example/androidapp/MoviesFragment.java
+++ app/src/main/java/com/example/androidapp/MoviesFragment.java
@@ -2,23 +2,25 @@ package com.example.androidapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
+
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.List;
 
 public class MoviesFragment extends Fragment {
+    View view;
+    RecyclerView recyclerView;
+    MovieAdapter movieAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.movies_fragment, container, false);
+        view = inflater.inflate(R.layout.movies_fragment, container, false);
 
-        RecyclerView recyclerView = view.findViewById(R.id.movies_recycler_view);
+        recyclerView = view.findViewById(R.id.movies_recycler_view);
 
-        MovieAdapter movieAdapter = new MovieAdapter();
+        movieAdapter = new MovieAdapter();
         recyclerView.setAdapter(movieAdapter);
         movieAdapter.setMovies(new TestMoviesRepositoryImpl().getMovies());
 
