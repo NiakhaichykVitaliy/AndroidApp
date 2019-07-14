ed314ca6f800d623146e4e954deec3a4149fa27a
NiakhaichykVitaliy <2603hbo@gmail.com>
2019-07-14 21:38:42 +0300

    Update Glide

--- app/src/main/java/com/example/androidapp/data/GlideImageLoader.java
+++ app/src/main/java/com/example/androidapp/data/GlideImageLoader.java
@@ -3,12 +3,17 @@ package com.example.androidapp.data;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.example.androidapp.R;
 
 
 public class GlideImageLoader implements ImageLoader {
 
     @Override
     public void loadImage(String imageUrl, ImageView imageView) {
-        Glide.with(imageView).load(imageUrl).into(imageView);
+        Glide.with(imageView)
+                .load(imageUrl)
+                .centerCrop()
+                .placeholder(R.drawable.ic_loading)
+                .into(imageView);
     }
 }
