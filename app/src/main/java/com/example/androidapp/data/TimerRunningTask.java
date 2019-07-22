99dd632ccc2935f4b9bc0937caf582a546cb968a
NiakhaichykVitaliy <2603hbo@gmail.com>
2019-07-22 15:57:02 +0300

    change variables timeRunning from global into local type

--- app/src/main/java/com/example/androidapp/data/TimerRunningTask.java
+++ app/src/main/java/com/example/androidapp/data/TimerRunningTask.java
@@ -7,13 +7,12 @@ import java.util.TimerTask;
 import static android.support.constraint.Constraints.TAG;
 
 public class TimerRunningTask extends TimerTask {
-    private long timeRunning = 0;
     private long startTime = System.currentTimeMillis();
 
     @Override
     public void run() {
         long currentTime = System.currentTimeMillis();
-        timeRunning = (currentTime - startTime) / 1000;
+        long timeRunning = (currentTime - startTime) / 1000;
         Log.i(TAG, timeRunning + " sec");
     }
 }
--- app/src/main/java/com/example/androidapp/ui/MainActivity.java
+++ app/src/main/java/com/example/androidapp/ui/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.androidapp.ui;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.androidapp.R;
-import com.example.androidapp.services.TimerService;
 
 public class MainActivity extends AppCompatActivity {
 
